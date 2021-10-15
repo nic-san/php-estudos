2068979fd26b73106641806e141777e37b93c07c
Nicolas Tavares dos Santos <77157262+nic-san@users.noreply.github.com>
2021-10-15 14:07:39 -0300

    Add files via upload

---
+++ sessoes-cookies-php/fase1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+
+/* 
+$_SESSION['usuario'] = [
+    'nome'=>'Nicolas',
+    'idade'=> 17,
+    'ativo'=>true,
+    'rate' => 4.3
+];
+*/
+
+//unset($_SESSION['usuario']);
+
+$_SESSION['usuario'] = 'asd';
+$_SESSION['ultimo-acesso'] = '12-12-2012';
+$_SESSION['id-user'] = 3;
---
+++ sessoes-cookies-php/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__.'/session.php';
+
+$user = $_SESSION ['user'] ?? null;
+
+if (!$user){
+    header('location: login.php');
+    exit;
+}
+
+?>
+<h1>Página protegida</h1>
+
+<p>Olá, <?php echo $user['email']; ?></p>
